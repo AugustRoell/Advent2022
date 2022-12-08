a0b249913f974e84dc8f0f28f468ef26a85c0177
AugustRoell <aroeell@student.ethz.ch>
2022-12-08 13:33:10 +0100

    Day 8

---
+++ advent8/advent8_1.cpp
@@ -0,0 +1,137 @@
+//Day 8 of Advent of Code 2022
+//
+//A program to find the total number of trees visible from the outside of a 
+//grid of trees, given the positions and heights of the trees in this grid. 
+//A tree is considered visible from the outside if there is at least one 
+//direction along its row or column (upward, downward, to the right, or to the 
+//left, but not diagonal) such that there are no trees of greater or equal 
+//height there.
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+//A function that takes as its arguments a grid of trees (represented by a 
+//matrix of unsigned integers), as well as two insigned integers 
+//representing the row and column of a specific tree. It will return "true" if 
+//that specific tree is visible from the outside, and "false" if it is not.
+//
+//This is done by two separate itterations, one over the row, the other over 
+//the column of the specific tree. If a tree of equal or greater height is 
+//found, it is noted whether this tree is to the right, left, top, or bottom 
+//of the original tree. Finally, if the tree is not visible from any of these 
+//four directions, the function returns "false", and otherwise it 
+//returns "true".
+bool isVisible(std::vector<std::vector<unsigned int>> grid, 
+		unsigned int row, unsigned int column) {
+
+	unsigned int num_of_rows = grid.size(), 
+		num_of_columns = grid[0].size();
+
+	bool visible_from_right = true, 
+		visible_from_left = true,
+		visible_from_top = true,
+		visible_from_bottom = true;
+
+	for (unsigned int column_number = 0; 
+			column_number < num_of_columns; 
+			column_number++) {
+		
+		if (column_number != column) {
+			if (grid[row][column_number] >= 
+					grid[row][column]) {
+				
+				if (column_number > column) {
+					visible_from_right = false;
+				}
+					
+				else {
+					visible_from_left = false;
+				}
+			}
+		}
+	}
+
+	for (unsigned int row_number = 0; 
+			row_number < num_of_rows; 
+			row_number++) {
+		
+		if (row_number != row) {
+			if (grid[row_number][column] >= 
+					grid[row][column]) {
+				
+				if (row_number > row) {
+					visible_from_bottom = false;
+				}
+					
+				else {
+					visible_from_top = false;
+				}
+			}
+		}
+	}
+
+	return (visible_from_right || 
+		visible_from_left || 
+		visible_from_top || 
+		visible_from_bottom);
+}
+
+
+int main() {
+
+	//A string object that will contain the input lines.
+	std::string line;
+
+	//Three unsigned integer objects, the first two of which specify the 
+	//dimensions of the grid of trees, and the third of which keeps track 
+	//of the number of visible trees in the grid.
+	unsigned int num_of_rows = 99, num_of_columns = 99, 
+		     num_of_visible_trees = 0;
+
+	//A vector object, containing vectors of unsigned integers, so that 
+	//the whole object represents the grid of trees, with each integer 
+	//representing the height of a specific tree. It is initialized with 
+	//the dimensions given by "num_of_rows" and "num_of_columns".
+	std::vector<std::vector<unsigned int>> grid (num_of_rows, 
+			std::vector<unsigned int> (num_of_columns));
+
+
+	//Here we read the input .txt file per line.
+	//
+	//For each input line, we convert each of the characters in the string 
+	//to an (unsigned) integer and write it to the corresponding element 
+	//of the "grid" object. In this way we construct the "grid" objects, 
+	//representing the grid of trees given by the input .txt file.
+	for (unsigned int i = 0; i < num_of_rows; i++) {
+
+		std::cin >> line;
+
+		for (unsigned int j = 0; j < num_of_columns; j++) {
+			grid[i][j] = stoi(line.substr(j, 1));
+		}
+	}
+
+
+	//Here we itterate over the entire "grid" object and check for each 
+	//element, corresponding to a single tree in the grid of trees,
+	//whether this tree would be visible from the outside, which is done 
+	//by applying the "isVisbile" function. If a tree is visible from the 
+	//outside, we increase the "num_of_visible_trees" integer by one.
+	for (unsigned int row = 0; row < num_of_rows; row++) {
+		for (unsigned int column = 0; 
+				column < num_of_columns; column++) {
+		
+			if (isVisible(grid, row, column)) {
+				num_of_visible_trees++;
+			}
+		}
+	}
+
+	std::cout << "There are " << num_of_visible_trees 
+		<< " trees visibile from the outside of the grid" << '\n';
+
+	return 0;
+}
---
+++ advent8/advent8_2.cpp
@@ -0,0 +1,153 @@
+//Day 8 of Advent of Code 2022
+//
+//A program to find the largest scenic score of any tree in a grid of trees, 
+//given the positions and heights of the trees in the grid. The scenic score 
+//of a tree is calculated by the product of the viewing distances along the 
+//axes of the grid. The viewing distance along any direction is the number of 
+//trees that can be seen in that direction, so that it is 1 plus the number of 
+//steps in that direction untill a tree of equal or greater height is found. 
+//The 1 is added because the tree of equal or greater height is also visible. 
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+//A function that takes as its arguments a grid of trees (represented by a 
+//matrix of unsigned integers), as well as two insigned integers 
+//representing the row and column of a specific tree. It will return and 
+//unsigned integer representing the scenic score of that tree.
+//
+//This is done by four separate itterations, towards the right, left, top, and 
+//bottom of the specific tree. In each of these directions, we find how many 
+//trees are visible from our starting tree. Finally, we output the product of 
+//these numbers of visible trees in all four directions to obtain the scenic 
+//score. Note that we first check if the tree is at the edge of the grid 
+//("row" or "column" equal to zero), because in that case we know its scenic 
+//score will be zero without calculating all the viewing distances.
+unsigned int ScenicScore(std::vector<std::vector<unsigned int>> grid, 
+		unsigned int row, unsigned int column) {
+
+	if (row == 0 || column == 0) {
+		return 0;
+	}
+
+	unsigned int num_of_rows = grid.size(), 
+		num_of_columns = grid[0].size(),
+		visible_to_right = 0, 
+		visible_to_left = 0,
+		visible_to_top = 0,
+		visible_to_bottom = 0;
+
+	for (unsigned int column_number = column + 1;
+			column_number < num_of_columns; 
+			column_number++) {
+			
+		visible_to_right++;
+
+		if (grid[row][column_number] >= grid[row][column]) {
+			break;
+		}
+	}
+
+	for (int column_number = column - 1;
+			column_number >= 0; 
+			column_number--) {
+		
+		visible_to_left++;
+
+		if (grid[row][column_number] >= grid[row][column]) {
+			break;
+		}
+	}
+
+	for (int row_number = row - 1;
+			row_number >= 0; 
+			row_number--) {
+		
+		visible_to_top++;
+
+		if (grid[row_number][column] >= grid[row][column]) {
+			break;
+		}
+	}
+
+	for (unsigned int row_number = row + 1;
+			row_number < num_of_rows; 
+			row_number++) {
+		
+		visible_to_bottom++;
+
+		if (grid[row_number][column] >= grid[row][column]) {
+			break;
+		}
+	}
+
+	return (visible_to_right * 
+		visible_to_left * 
+		visible_to_top * 
+		visible_to_bottom);
+
+}
+
+
+int main() {
+
+	//A string object that will contain the input lines.
+	std::string line;
+
+	//Four unsigned integer objects, the first two of which specify the 
+	//dimensions of the grid of trees, and the second two of which 
+	//respectively keep track of the scenic score of each tree and the 
+	//largest scenic score yet encountered.
+	unsigned int num_of_rows = 99, num_of_columns = 99, 
+		     new_scenic_score,
+		     max_scenic_score = 0;
+
+	//A vector object, containing vectors of unsigned integers, so that 
+	//the whole object represents the grid of trees, with each integer 
+	//representing the height of a specific tree. It is initialized with 
+	//the dimensions given by "num_of_rows" and "num_of_columns".
+	std::vector<std::vector<unsigned int>> grid (num_of_rows, 
+			std::vector<unsigned int> (num_of_columns));
+
+
+	//Here we read the input .txt file per line.
+	//
+	//For each input line, we convert each of the characters in the string 
+	//to an (unsigned) integer and write it to the corresponding element 
+	//of the "grid" object. In this way we construct the "grid" objects, 
+	//representing the grid of trees given by the input .txt file.
+	for (unsigned int i = 0; i < num_of_rows; i++) {
+
+		std::cin >> line;
+
+		for (unsigned int j = 0; j < num_of_columns; j++) {
+			grid[i][j] = stoi(line.substr(j, 1));
+		}
+	}
+
+
+	//Here we itterate over the entire "grid" object and check for each 
+	//element, corresponding to a single tree in the grid of trees,
+	//whether the scenic score of this tree is greater than the greatest 
+	//scenic score yet encountered. If it is, it becomes the new value of 
+	//"max_scenic_score".
+	for (unsigned int row = 0; row < num_of_rows; row++) {
+		for (unsigned int column = 0; 
+				column < num_of_columns; column++) {
+			
+			new_scenic_score = ScenicScore(grid, row, column);
+
+			if (new_scenic_score > max_scenic_score) {
+				max_scenic_score = new_scenic_score;
+			}
+		}
+	}
+
+	std::cout << "The largest scenic score in this grid of trees is " 
+		<< max_scenic_score << '\n';
+
+	return 0;
+}
